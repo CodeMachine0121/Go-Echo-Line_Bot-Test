ade745891cafd15652597b585744ec7b099756d2
james.hsueh77 <james.hsueh@titansoft.com.sg>
2023-08-04 01:08:20 +0800

    change conf

--- api/main.go
+++ api/main.go
@@ -8,17 +8,15 @@ import (
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
-func Handle(w http.ResponseWriter, r *http.Request) {
+var bot *linebot.Client
 
+func init() {
 	channel_access_token := os.Getenv("CHANNEL_ACCESS_TOKEN")
 	channel_secret := os.Getenv("CHANNEL_SECRET")
+	bot, _ = linebot.New(channel_secret, channel_access_token)
+}
 
-	bot, err := linebot.New(channel_secret, channel_access_token)
-
-	if err != nil {
-		log.Fatal("error occurred while creating")
-	}
-
+func Handle(w http.ResponseWriter, r *http.Request) {
 	events, _ := bot.ParseRequest(r)
 
 	for _, event := range events {
